7a0acc6535f4b2adf6651fd19bd969395d7f2672
Futaba-Kosuke <k.futaba2828@gmail.com>
2021-12-11 18:08:56 +0900

    Add: マス目分類(#4)のモックを実装 (#13)

---
+++ classify_shogi_piece/main.py
@@ -0,0 +1,40 @@
+from typing import Any, Final, List
+
+import numpy as np
+import numpy.typing as npt
+
+IntegerArrayType: Final[Any] = npt.NDArray[np.int_]
+
+SHOGI_PIECES: List[str] = [
+    "玉",
+    "金",
+    "銀",
+    "桂",
+    "香",
+    "角",
+    "飛",
+    "歩",
+    "成銀",
+    "成桂",
+    "成香",
+    "馬",
+    "竜",
+]
+
+
+def mock_classify_pieces(
+    image: IntegerArrayType, model_path: str
+) -> IntegerArrayType:
+    return np.array(
+        [
+            [-5, -4, -3, -2, -1, -2, -3, -4, -5],
+            [0, -6, 0, 0, 0, 0, 0, -7, 0],
+            [-8, -8, -8, -8, -8, -8, -8, -8, -8],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [8, 8, 8, 8, 8, 8, 8, 8, 8],
+            [0, 6, 0, 0, 0, 0, 0, 7, 0],
+            [5, 4, 3, 2, 1, 2, 3, 4, 5],
+        ]
+    )
