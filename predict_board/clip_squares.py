fc6eb2e640e74587603a4d56cdc64bd7eb15e800
Futaba-Kosuke <k.futaba2828@gmail.com>
2021-12-13 00:12:03 +0900

    Add: マス目の切り抜き処理を実装 (#3)

--- predict_board/clip_squares.py
+++ predict_board/clip_squares.py
@@ -13,8 +13,11 @@ BASE_SIZE: Final[int] = 63
 def clip_squares(
     image: IntegerArrayType, corners: IntegerArrayType
 ) -> IntegerArrayType:
-    board_image = get_board_image(image=image, corners=corners)
-    print(board_image)
+    board_image: IntegerArrayType = get_board_image(
+        image=image, corners=corners
+    )
+    squares: IntegerArrayType = get_squares(image=board_image)
+    return squares
 
 
 def get_board_image(
@@ -32,17 +35,45 @@ def get_board_image(
     return result
 
 
-if __name__ == "__main__":
-    image: IntegerArrayType = cv2.imread("./data/test.jpg")
-    board_image: IntegerArrayType = get_board_image(
-        image=image,
-        corners=np.array(
-            [[[758, 606]], [[30, 690]], [[118, 136]], [[610, 110]]]
-        ),
+def get_squares(image: IntegerArrayType) -> IntegerArrayType:
+    width, height, _ = image.shape
+    square_width: int = int(width / 9)
+    square_height: int = int(height / 9)
+
+    squares: IntegerArrayType = np.array(
+        [
+            image[
+                j * square_width : (j + 1) * square_width,
+                i * square_height : (i + 1) * square_height,
+                :,
+            ]
+            for j in range(9)
+            for i in range(9)
+        ]
     )
 
-    print(board_image.shape, board_image.dtype)
+    return squares
 
-    cv2.imshow("corners", board_image)
+
+if __name__ == "__main__":
+    image: IntegerArrayType = cv2.imread("./data/test.jpg")
+    # cornersで検出されたものを登録する
+    corners: IntegerArrayType = np.array(
+        [[[758, 606]], [[30, 690]], [[118, 136]], [[610, 110]]]
+    )
+    board_image: IntegerArrayType = get_board_image(
+        image=image, corners=corners
+    )
+    square_images: IntegerArrayType = clip_squares(
+        image=image, corners=corners
+    )
+
+    print(square_images.shape, square_images.dtype)
+
+    cv2.imshow("board", board_image)
+    cv2.imshow("squares", np.vstack(square_images))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+    cv2.imwrite("./data/board.jpg", board_image)
+    cv2.imwrite("./data/square.jpg", np.vstack(square_images))
