19577531dbebb3c7f622670b20f3ed39b24581ae
taisei12232 <taisei12232@gmail.com>
2021-12-17 01:54:56 +0900

    Add: 全パターンの棋譜作成処理を実装 (#5)

--- generate_kifu/main.py
+++ generate_kifu/main.py
@@ -33,60 +33,44 @@ def mock_generate_kifu(pieces: IntegerArrayType, is_sente: bool) -> str:
     before_pieces = np.loadtxt(
         "../data/prev_board.csv", delimiter=",", dtype="int64"
     )
-    diff_pieces = np.where(pieces != before_pieces)
-    kifu: str = ""
-    if pieces[diff_pieces].size == 0:
-        print("変更ナシ")
-    elif pieces[diff_pieces].size == 2:
-        diff_coords = [
-            list(diff_pieces[np.where(pieces[diff_pieces] != 0)[0][0]]),
-            list(diff_pieces[np.where(pieces[diff_pieces] == 0)[0][0]]),
-        ]
-        before_move = list(before_pieces[diff_coords])
+    if is_sente:
+        before_move = list(
+            zip(*np.where((pieces != before_pieces) & (before_pieces > 0)))
+        )
         after_move = list(
-            [
-                pieces[diff_coords[0][0]][diff_coords[0][1]],
-                pieces[diff_coords[1][0]][diff_coords[1][1]],
-            ]
+            zip(*np.where((pieces != before_pieces) & (before_pieces <= 0)))
         )
-        print(
-            [
-                [diff_coords[0][0]],
-                [diff_coords[0][1]],
-                [diff_coords[1][0]],
-                [diff_coords[1][1]],
-            ]
-        )
-        print(before_move)
-        print(after_move)
-
-        print(diff_coords[0])
-        print(diff_coords)
-        print(pieces[5][6])
-        kifu = (
-            str(9 - diff_coords[0][1])
-            + KAN_SUJI[diff_coords[0][0]]
-            + SHOGI_PIECES[abs(after_move[0]) - 1]
-        )
-        if before_move[1] != after_move[0]:
-            kifu += "成"
-        kifu += (
-            "("
-            + str(9 - diff_coords[1][1] + 1)
-            + str(diff_coords[1][0] + 1)
-            + ")"
-        )
-        print(kifu)
     else:
-        diff_coords = list(zip(*diff_pieces))
+        before_move = list(
+            zip(*np.where((pieces != before_pieces) & (before_pieces < 0)))
+        )
+        after_move = list(
+            zip(*np.where((pieces != before_pieces) & (before_pieces >= 0)))
+        )
+    kifu: str = ""
+    if len(after_move) == 0:
+        print("変更ナシ")
+    elif len(before_move) == 0:
+        diff_coords = list(zip(*(before_pieces != pieces)))
         kifu = (
             str(9 - diff_coords[0][1])
             + KAN_SUJI[diff_coords[0][0]]
-            # + SHOGI_PIECES[abs(pieces[diff_coords[0]]) - 1]
+            + SHOGI_PIECES[abs(pieces[diff_coords[0]]) - 1]
             + "打"
         )
-        print(kifu)
-    # np.savetxt("../data/prev_board.csv", pieces, delimiter=",", fmt='%d')
+    else:
+        kifu = (
+            str(9 - after_move[0][1])
+            + KAN_SUJI[after_move[0][0]]
+            + SHOGI_PIECES[abs(pieces[after_move[0]]) - 1]
+        )
+        if before_pieces[before_move[0]] != pieces[after_move[0]]:
+            kifu += "成"
+        kifu += (
+            "(" + str(9 - before_move[0][1]) + str(before_move[0][0] + 1) + ")"
+        )
+    print(kifu)
+    np.savetxt("../data/prev_board.csv", pieces, delimiter=",", fmt="%d")
     return kifu
 
 
@@ -95,11 +79,11 @@ piece = np.array(
         [-5, -4, -3, -2, 1, -2, -3, -4, -5],
         [0, -6, 0, 0, 0, 0, 0, -7, 0],
         [-8, -8, -8, -8, -8, 0, -8, -8, -8],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 8, 0, 0],
-        [0, 0, 8, 0, 8, 8, 0, 0, 0],
-        [8, 8, 6, 8, 0, 0, 0, 8, 8],
-        [0, 0, 0, 0, 8, 0, 0, 7, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 8, 6, 0, 0, 0, 8, 0],
+        [8, 8, 0, 8, 8, 8, 0, 0, 8],
+        [0, 0, 0, 0, 0, 0, 0, 7, 0],
         [5, 4, 3, 2, 1, 2, 3, 4, 5],
     ]
 )
