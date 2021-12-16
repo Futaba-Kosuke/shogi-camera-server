14f149e0bdf71e5aa4386d56c38e33e9312e5cc2
taisei12232 <taisei12232@gmail.com>
2021-12-17 02:26:17 +0900

    Update: 文字列の連結方式をf-stringに変更 (#5)

--- generate_kifu/main.py
+++ generate_kifu/main.py
@@ -51,40 +51,38 @@ def mock_generate_kifu(pieces: IntegerArrayType, is_sente: bool) -> str:
     if len(after_move) == 0:
         print("変更ナシ")
     elif len(before_move) == 0:
-        diff_coords = list(zip(*(before_pieces != pieces)))
-        kifu = (
-            str(9 - diff_coords[0][1])
-            + KAN_SUJI[diff_coords[0][0]]
-            + SHOGI_PIECES[abs(pieces[diff_coords[0]]) - 1]
-            + "打"
-        )
+        diff_coords = list(zip(*(np.where(before_pieces != pieces))))
+        kifu = f"""{
+            9-diff_coords[0][1]}{
+            KAN_SUJI[diff_coords[0][0]]}{
+            SHOGI_PIECES[abs(pieces[diff_coords[0]]) - 1]}打"""
     else:
-        kifu = (
-            str(9 - after_move[0][1])
-            + KAN_SUJI[after_move[0][0]]
-            + SHOGI_PIECES[abs(pieces[after_move[0]]) - 1]
-        )
+        kifu = f"""{
+            9 - after_move[0][1]}{
+            KAN_SUJI[after_move[0][0]]}"""
         if before_pieces[before_move[0]] != pieces[after_move[0]]:
-            kifu += "成"
-        kifu += (
-            "(" + str(9 - before_move[0][1]) + str(before_move[0][0] + 1) + ")"
-        )
+            kifu += f"""{
+            SHOGI_PIECES[abs(before_pieces[before_move[0]]) - 1]}成"""
+        else:
+            kifu += f"{SHOGI_PIECES[abs(pieces[after_move[0]]) - 1]}"
+        kifu += f"({9 - before_move[0][1]}{before_move[0][0] + 1})"
     print(kifu)
     np.savetxt("../data/prev_board.csv", pieces, delimiter=",", fmt="%d")
     return kifu
 
 
-piece = np.array(
-    [
-        [-5, -4, -3, -2, 1, -2, -3, -4, -5],
-        [0, -6, 0, 0, 0, 0, 0, -7, 0],
-        [-8, -8, -8, -8, 0, 0, -8, -8, -8],
-        [0, 0, 0, 0, 0, 0, 8, 0, 0],
-        [0, 0, 0, 0, -8, 0, 0, 0, 0],
-        [0, 0, 8, 6, 0, 0, 0, 8, 0],
-        [8, 8, 0, 8, 8, 8, 0, 0, 8],
-        [0, 0, 0, 0, 0, 0, 0, 7, 0],
-        [5, 4, 3, 2, 1, 2, 3, 4, 5],
-    ]
-)
-mock_generate_kifu(piece, False)
+if __name__ == "__main__":
+    piece = np.array(
+        [
+            [-5, -4, -3, -2, 1, -2, -3, -4, -5],
+            [0, -6, 0, 0, 0, 0, 0, -7, 0],
+            [-8, -8, -8, -8, 0, 0, -8, -8, -8],
+            [0, 14, 8, 0, 0, 0, 8, 0, 0],
+            [0, 0, 0, 0, -8, 0, 0, 0, 0],
+            [8, 14, 0, 6, 0, 8, 8, 8, 0],
+            [0, 8, 0, 8, 8, 8, 0, 0, 8],
+            [8, 0, 0, 0, 0, 0, 0, 7, 0],
+            [5, 4, 3, 2, 1, 2, 3, 4, 5],
+        ]
+    )
+    mock_generate_kifu(piece, True)
